14d0dc062f5fd726601f2120f785d3acff7b738a
Gabriel Fernandes <gabgui2001@gmail.com>
2022-01-20 01:00:04 -0300

    .

--- Fundamentos do JS - Arrays/callbacks.js
+++ Fundamentos do JS - Arrays/callbacks.js
@@ -1,7 +1,7 @@
 const nome = ["Ana", "Pampolha", "Leo", "Jhun"]
-
-nome.forEach(ImprimeNomes)
+//callback(assicrona) não é executado na hora, apenas quando o usuario responder
 
 function ImprimeNomes(nome){
     console.log(nome)
 }
+nome.forEach(ImprimeNomes,posicao)
