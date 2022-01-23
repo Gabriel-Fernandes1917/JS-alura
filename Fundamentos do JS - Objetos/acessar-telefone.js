927737233603c3243acf399f85dde61043c04251
Gabriel Fernandes <gabgui2001@gmail.com>
2022-01-23 20:59:21 -0300

    .

---
+++ Fundamentos do JS - Objetos/acessar-telefone.js
@@ -0,0 +1,13 @@
+
+const cliente = {
+    nome: "Andre",
+    sobrenome :"Souza",
+    idade: 36,
+    cpf: "12121223434",
+    email: "edson@cesupa.com",
+    //array in tho object
+    fones:["5592323232323", "558923232323"]
+}
+
+cliente.fones.forEach(i => console.log(i))
+
