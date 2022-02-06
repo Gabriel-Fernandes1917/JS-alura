d930a850929a29131c4e2e8ecff239b059ea34b5
Gabriel Fernandes <gabgui2001@gmail.com>
2022-02-05 23:23:09 -0300

    .

---
+++ Algoritimos de ordenação/lista-livros.js
@@ -0,0 +1,23 @@
+const precosLivros = [{
+    titulo: "JS",
+    preco: 25
+}, {
+    titulo: "php",
+    preco: 15
+}, {
+    titulo: "Java",
+    preco: 30
+}, {
+    titulo: "Elixir",
+    preco: 50
+} ,{
+    titulo: "Go",
+    preco: 45
+},
+{
+    titulo: "Python",
+    preco: 20
+}
+]
+
+module.exports = precosLivros
